3a368bc68fbcca97cba3e70310bbf7746907ee3c
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-11 13:14:07 -0300

    Treinando as variáveis do JavaScript

--- Curso_Web_Moderno_JAVASCRIPT/Fundamentos/variaveiseconstantes.js
+++ Curso_Web_Moderno_JAVASCRIPT/Fundamentos/variaveiseconstantes.js
@@ -7,4 +7,27 @@ b = 40
 console.log(a,b)
 
 const c = 50
-c = 80
+//c = 80
+
+let a1 = 1000
+console.log(a1)
+
+let qualquer = 'legal'
+
+console.log(qualquer)
+
+console.log(typeof qualquer)
+
+qualquer = 10000000
+
+console.log(qualquer)
+
+console.log(typeof(qualquer))
+
+// evitar nomes genéricos para variáveis e constantes.
+
+let valor = ''
+let numero = 1
+let pqp = false //produto quimico perigoso.
+
+console.log(pqp)
