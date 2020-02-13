cf6d361620c72ad26c9539045d90f0b8eaf9bbfa
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-13 13:29:26 -0300

    Object no puro sem class para instaciar

---
+++ Curso_Web_Moderno_JAVASCRIPT/Fundamentos/Object.js
@@ -0,0 +1,24 @@
+// JavaScript Object Notations
+// Um objeto JavaScript não é JSON
+
+// JSON é um Formato Textual.
+
+const pessoa = {}
+
+pessoa.nome = 'Victor Pajeú'
+pessoa.idade = 20.6
+
+pessoa.idade = Number.parseInt(pessoa.idade)
+
+console.log(pessoa.idade)
+
+
+const produt = {
+    nome:'Camisa Polo',
+    preço:123.56
+}
+
+console.log('Produto: '+ produt)
+
+
+// JSON é uma formato textual, comunicação entre sistemas
