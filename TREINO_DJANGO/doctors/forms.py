4ac3eaccdc7937739ad8f2f301aaaf139115de8d
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-28 07:40:14 -0300

    Organização

--- TREINO_DJANGO/doctors/forms.py
+++
@@ -1,8 +0,0 @@
-from django import forms
-from .models import Doctor
-
-
-class DoctorForms(forms.ModelForm):
-    class Meta:
-        model = Doctor
-        fields = ['name', 'age', 'cpf']
--- TREINO_DJANGO/doctors/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-
-class Doctor(models.Model):
-    name = models.CharField('Nome', null=False, max_length=200)
-    age = models.IntegerField('Idade', null=False, )
-    cpf = models.IntegerField('CPF', null=False)
-
