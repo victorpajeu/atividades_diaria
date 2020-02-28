c578cb59fdd0773514207aa169485439cf10a1ed
victorhugobezerramoreirapajeu <pajeuvictor@gmail.com>
2020-02-28 08:21:16 -0300

    Criando o Doctor list mais criação

--- TREINO_DJANGO/clinca/doctors/views.py
+++ TREINO_DJANGO/clinca/doctors/views.py
@@ -1,14 +1,25 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, CreateView, UpdateView, DeleteView, TemplateView
+from django.views.generic import ListView, UpdateView, DeleteView, TemplateView
 from .models import Doctor
 from .forms import DoctorForms
 
 
-class ListDoctor(ListView):
-    model = Doctor
-    context_object_name = 'doctors'
+class ListDoctor(TemplateView):
+    template_name = 'doctors/doctor_form.html'
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['doctors'] = Doctor.objects.all()
+        context['form'] = DoctorForms(self.request.POST or None)
+        return context
+
+    def post(self, request, *args, **kwargs):
+        context = self.get_context_data(**kwargs)
+        form = context['form']
+        if form.is_valid():
+            form.save()
+            redirect(reverse_lazy('list'))
 
 
 class DoctorUpdate(UpdateView):
